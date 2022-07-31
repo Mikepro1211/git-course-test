31350940945beae4f6629efd1bafd5491502de43
Mikepro1211 <miguelalexander890@gmail.com>
2022-07-30 21:17:35 -0600

    Ahora el index html  tiene estructura html

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log ("Hola Mundo");
+const currentTIme =() =>{
+    const el = document.querySelector("h1");
+
+    let date = new date ();
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+
+    hh==hh <10 ?'0${hh}' : hh;
+    mm==mm <10 ? '0$'
+}
